dd0c9053a41af812c21e5f3d740f3e9b30bb64e6
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2018-11-16 16:58:51 -0200

    Adiciona form de insercao

--- Caelum/Blog/Controllers/PostController.cs
+++ Caelum/Blog/Controllers/PostController.cs
@@ -6,16 +6,34 @@ namespace Blog.Controllers
 {
     public class PostController : Controller
     {
+        private IList<Post> lista;
+
+        public PostController()
+        {
+            this.lista = new List<Post>{
+                new Post { Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
+                new Post { Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
+                new Post { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
+                new Post { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" }
+            };
+        }
+
         public IActionResult Index()
         {
-            var listaDePosts = new List<Post>()
-            {
-                new Post() { Titulo = "Harry Potter 1", Resumo = "Pedra Filosofal", Categoria = "Filme, Livro" },
-                new Post() { Titulo = "Cassino Royale", Resumo = "007", Categoria = "Filme" },
-                new Post() { Titulo = "Monge e o Executivo", Resumo = "Romance sobre Liderança", Categoria = "Livro" },
-                new Post() { Titulo = "New York, New York", Resumo = "Sucesso de Frank Sinatra", Categoria = "Música" }
-            };
-            return View(listaDePosts);
+            return View(lista);
         }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        public IActionResult Adiciona(Post post)
+        {
+            lista.Add(post);
+            return View("Index", lista);
+        }
+
+
     }
 }
